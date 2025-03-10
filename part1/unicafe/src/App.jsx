2e0196c36b1ec89ef2d8f1d8015240d37f9fdb5e
daljit-brainomix <dsingh@brainomix.com>
2025-03-10 11:50:49 +0000

    Part1-Unicafe - Exercise 1.8 unicafe step 3. Use Statistics component.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,49 +1,60 @@
 import { useState } from 'react'
 
-
 const Button = (props) => {
   return <>
     <button onClick={props.onClick}>{props.text}</button>
   </>
 }
+
 const DisplayStat = (props) => {
   return <>
     <p><strong>{props.text}:</strong> {props.feedback}</p>
   </>
 }
 
+const Statistics = (props) => {
+  
+  const total = props.good + props.neutral + props.bad 
+  const averageScore = ((props.good*1)+(props.neutral*0)+(props.bad*-1))/3
+  let positivePercentage = 0
+  if(total > 0) {
+    positivePercentage = (props.good/total)*100
+  }
+
+  if(total < 1) {
+    return <>
+      <p>No feedback given.</p>
+    </>
+  } else {
+    return <>
+      <DisplayStat feedback={props.good} text="Good" />
+      <DisplayStat feedback={props.neutral} text="Neutral" />
+      <DisplayStat feedback={props.bad} text="Bad" />
+
+      <DisplayStat feedback={total} text="Feedback" />
+      
+      <DisplayStat feedback={averageScore} text="Average Score" />
+      <DisplayStat feedback={positivePercentage+"%"} text="Positive feedback" />    
+    </>
+  }
+}
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
-  const [averageScore, setAverageScore] = useState(0)
-  const [positivePercentage, setPositivePercentage] = useState(0)
 
   const handleGood = () => {
     const updatedGood = good+1
-    const updatedTotal = total+1
     setGood(updatedGood)
-    setTotal(updatedTotal)
-    setAverageScore(((updatedGood*1)+(neutral*0)+(bad*-1))/3)
-    setPositivePercentage((updatedGood/updatedTotal)*100)
   }
   const handleNeutral = () => {
     const updatedNeutral = neutral+1
-    const updatedTotal = total+1
     setNeutral(updatedNeutral)
-    setTotal(updatedTotal)    
-    setAverageScore(((good*1)+(updatedNeutral*0)+(bad*-1))/3)
-    setPositivePercentage((good/updatedTotal)*100)
   }
   const handleBad = () => {
     const updatedBad = bad+1
-    const updatedTotal = total+1
     setBad(updatedBad)
-    setTotal(updatedTotal)    
-    setAverageScore(((good*1)+(neutral*0)+(updatedBad*-1))/3)    
-    setPositivePercentage((good/updatedTotal)*100)
   }
 
   return (
@@ -54,14 +65,7 @@ const App = () => {
       <Button onClick={handleBad} text="Bad" /> &nbsp;
 
       <h2>Statistics</h2>
-      <DisplayStat feedback={good} text="Total Good" />
-      <DisplayStat feedback={neutral} text="Total Neutral" />
-      <DisplayStat feedback={bad} text="Total Bad" />
-      <DisplayStat feedback={total} text="Total Feedback" />
-      
-      <DisplayStat feedback={averageScore} text="Average Score" />
-      <DisplayStat feedback={positivePercentage+"%"} text="Positive feedback" />
-
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
