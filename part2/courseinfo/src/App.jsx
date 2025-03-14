bde01a77ad211484052523174ea5d47cc7ea771c
daljit-brainomix <dsingh@brainomix.com>
2025-03-14 18:19:13 +0000

    Part2-Courseinfo - Ex 2.3 Step 8 - Using reduce for sum

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -17,17 +17,13 @@ const Part = (props) => (
 const Total = (props) => <p>Total of {props.total} exercises.</p>
 
 const Course = ({course}) => {
+  const total = course.parts.reduce((sum, part)=> sum + part.exercises, 0)
+  
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
-      <Total
-        total={
-          course.parts[0].exercises +
-          course.parts[1].exercises +
-          course.parts[2].exercises
-        }
-      />
+      <Total total={total} />
     </div>
   )
 }
