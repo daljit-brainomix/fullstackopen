9612bfc6f64ee6badce71224ba3ca5c83a051c61
daljit-brainomix <dsingh@brainomix.com>
2025-03-14 18:40:36 +0000

    Part2-Courseinfo - Ex 2.5 Step 10 - Course as separate module

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,31 +1,4 @@
-const Header = (props) => <h1>{props.course}</h1>
-
-const Content = ({parts}) => (
-  <div>
-    {parts.map((part) => <Part key={part.id} part={part} />)}
-  </div>
-)
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-  )
-
-
-const Total = (props) => <p>Total of {props.total} exercises.</p>
-
-const Course = ({course}) => {
-  const total = course.parts.reduce((sum, part)=> sum + part.exercises, 0)
-  
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total total={total} />
-    </div>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
