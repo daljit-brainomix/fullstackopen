4a2e16d5f121d7397089ec6c93f6cb5a5681278d
daljit-brainomix <dsingh@brainomix.com>
2025-04-16 15:39:19 +0100

    Part4-Blog - 4.2 Blog List, step 2 - final

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -1,17 +1,33 @@
-require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-const blogRouter = require('./controllers/blogs')
 
+const config = require('./utils/config')
+const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 
 const app = express()
 
-// Middleware for parsing JSON content
+logger.info(`Connecting to ${config.MONGODB_URI}`)
+// Connect to DB
+mongoose.connect(config.MONGODB_URI)
+.then(()=> logger.info('Connected to MongoDB'))
+.catch(() => logger.error('Failed to conenct to MongoDB'))
+
+// Middleware to load FE
+app.use(express.static('dist'))
+// Middleware to parse JSON content
 app.use(express.json())
+// Middleware to log requests
+app.use(middleware.requestLogger)
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
-
+// Register API routes
 app.use('/api/blogs', blogRouter)
+
+// Handle un-registered routes
+app.use(middleware.unknownEndpoint)
+// Handle application errors gracefully
+app.use(middleware.errorHandler)
+
 
 module.exports = app
