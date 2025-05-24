887b25fd39328098a9465155b9c6f9b8d19e44bd
daljit-brainomix <dsingh@brainomix.com>
2025-05-24 22:41:30 +0100

    Part5-Blog-Frontend - 5.1: Blog List Frontend, step 1

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +16,43 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    console.log("Logging in with", username, password)
+
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+
   return (
     <div>
-      <h2>blogs</h2>
+      <h2>Login</h2>
+      {
+        user === null ?
+          <LoginForm
+            username={username}
+            password={password}
+            setUsername={setUsername}
+            setPassword={setPassword}
+            handleLogin={handleLogin}
+          /> :
+          <div>
+            <p>{user.name} logged-in</p>
+          </div>
+      }
+      <hr />
+      <h2>blogs</h2>      
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
